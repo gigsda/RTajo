472ae337d22b25fe863782d262f5b4d96831756b
gigsda <gigsda@gmail.com>
2015-05-05 16:31:43 +0900

    add installed package test

--- RTajo/test/run_test.R
+++ RTajo/test/run_test.R
@@ -1,6 +1,4 @@
-library('rJava')
-library('RJDBC')
-source('..//R//RTajo.R')
+library('RTajo')
 rtajo.init(tajo_home='/usr/lib/tajo/', tajo_jdbc_driver_file ='/usr/lib/tajo/share/jdbc-dist/tajo-jdbc.jar');
 conn <- rtajo.connect(IP='localhost',port='26002');
 
